a80313973bcd405db00334c23cfbed2678cf3eef
kharismarizqii <rizqikharisma@gmail.com>
2020-10-13 21:00:36 +0700

    paging data adapter 7.0

--- app/src/main/java/com/kharismarizqii/movieapp/api/MovieApi.kt
+++ app/src/main/java/com/kharismarizqii/movieapp/api/MovieApi.kt
@@ -2,6 +2,7 @@ package com.kharismarizqii.movieapp.api
 
 import com.kharismarizqii.movieapp.BuildConfig
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.Query
 
@@ -12,9 +13,7 @@ interface MovieApi {
         const val API_KEY = BuildConfig.MOVIEDB_API_KEY
     }
 
-    @Headers("Authorization: $API_KEY")
-
-    @GET("search/movie")
+    @GET("search/movie?api_key=$API_KEY")
     suspend fun searchMovies(
         @Query("query") query: String,
         @Query("page") page: Int
--- app/src/main/java/com/kharismarizqii/movieapp/ui/catalog/MovieFragment.kt
+++ app/src/main/java/com/kharismarizqii/movieapp/ui/catalog/MovieFragment.kt
@@ -4,17 +4,33 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kharismarizqii.movieapp.R
+import com.kharismarizqii.movieapp.databinding.FragmentCatalogBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MovieFragment : Fragment(R.layout.fragment_catalog){
     private val viewModel by  viewModels<MovieViewModel>()
 
+    private var _binding : FragmentCatalogBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentCatalogBinding.bind(view)
+
+        val adapter = MovieAdapter()
+
+        binding.apply {
+            rvCatalog.setHasFixedSize(true)
+            rvCatalog.adapter = adapter
+        }
+
         viewModel.movies.observe(viewLifecycleOwner) {
-            
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
     }
 }
