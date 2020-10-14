53a08cbacd613b4d63ed7b902c0babbd56539415
kharismarizqii <rizqikharisma@gmail.com>
2020-10-14 11:32:53 +0700

    handle process death 13.0

--- app/src/main/java/com/kharismarizqii/movieapp/ui/catalog/MovieViewModel.kt
+++ app/src/main/java/com/kharismarizqii/movieapp/ui/catalog/MovieViewModel.kt
@@ -1,15 +1,15 @@
 package com.kharismarizqii.movieapp.ui.catalog
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.switchMap
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import androidx.paging.cachedIn
 import com.kharismarizqii.movieapp.data.MovieRepository
 
-class MovieViewModel @ViewModelInject constructor(private val repository: MovieRepository) : ViewModel(){
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+class MovieViewModel @ViewModelInject constructor(
+    private val repository: MovieRepository,
+    @Assisted state: SavedStateHandle) : ViewModel(){
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
     val movies = currentQuery.switchMap { query ->
         repository.getSearchResults(query).cachedIn(viewModelScope)
     }
@@ -19,6 +19,7 @@ class MovieViewModel @ViewModelInject constructor(private val repository: MovieR
     }
 
     companion object{
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "the"
     }
 }
